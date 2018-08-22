2fe1527ea07a969bf51d745773de23f54ce8586b
Yu-Igarashi-aiiit <39993912+Yu-Igarashi-aiiit@users.noreply.github.com>
2018-08-23 00:12:05 +0900

    Update Knapsack.c

--- Knapsack.c
+++ Knapsack.c
@@ -1,16 +1,23 @@
+define MAX_N = 100;
 int n,W;
 int x[MAX_N],v[MAX_N];
 
+
+
 int rec(int i,int j){
+  //i番目以降の品物から重さの総和がj以下となるよう選ぶ
   int res;
   if(i ==n){
-    //ここ
+    //もう品物が　残っていない
+    res=//ここ
   } else if(j < w[i]) {
-    //ここ
+    //この品物は入らない
+    res=//ここ
   }else {
+    //入れない場合と入れる場合を両方試す
       res = max(rec(i + 1 , j),rec(i + 1,j - w[i]) + v[i]);
   }
-    //ここ
+    return res;
 }
 
 void solve(){
