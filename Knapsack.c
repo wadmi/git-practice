3c0b1719aed70a8dc951187736e7bdbf1e20e9bf
wadmi <42609366+wadmi@users.noreply.github.com>
2018-08-23 00:19:21 +0900

    Update Knapsack.c

--- Knapsack.c
+++ Knapsack.c
@@ -9,10 +9,10 @@ int rec(int i,int j){
   int res;
   if(i ==n){
     //もう品物が　残っていない
-    res=//ここ
+    res=0;
   } else if(j < w[i]) {
     //この品物は入らない
-    res=//ここ
+    res=rec(i+1,j);
   }else {
     //入れない場合と入れる場合を両方試す
       res = max(rec(i + 1 , j),rec(i + 1,j - w[i]) + v[i]);
@@ -21,5 +21,5 @@ int rec(int i,int j){
 }
 
 void solve(){
-  printf("%d\n",rec(0,w));
+  printf("%d\n",rec(0,W));
 }
